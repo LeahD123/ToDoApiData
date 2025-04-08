74fa06ba6ff24a7b453e7eb3f1a84e535fc2ac35
LeahD <Lia.dachkov@gmail.com>
2025-04-08 15:58:36 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,87 @@
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+using TodoApi;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+// Added as service - הוספת שירותי שרת MYSQL
+builder.Services.AddDbContext<ToDoDbContext>(options =>
+    options.UseMySql("server=localhost;user=root;password=Lia120406;database=practicode", ServerVersion.AutoDetect("server=localhost;user=root;password=Lia120406;database=practicode")));
+
+
+
+// הוסף את CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
+
+
+var app = builder.Build();
+
+
+// השתמש במדיניות CORS
+app.UseCors("AllowAllOrigins");
+
+
+//בקשת GET - שולף מתוך הSQL דרך ההזרקה את כל הנתונים שיש שם, ומחבר אותם לליסט אחדט
+app.MapGet("/get", async (ToDoDbContext db) =>
+{
+    return await db.Items.ToListAsync();
+});
+
+
+
+
+app.MapPost("/post", async (Item todoItem, ToDoDbContext db) =>
+{
+    db.Items.Add(todoItem);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/post/{todoItem.Id}", todoItem);
+});
+
+
+
+app.MapPut("/put/{id}", async (int id, Item dto, ToDoDbContext db) =>
+{
+    var item = await db.Items.FindAsync(id);
+
+    if (item is null) return Results.NotFound();
+
+    item.Name = dto.Name;
+    item.IsComplete = dto.IsComplete; // הנח שהשדה הזה קיים במחלקת Item
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/delete/{id}", async (int id, ToDoDbContext db) =>
+{
+    var item = await db.Items.FindAsync(id);
+    if (item != null)
+    {
+        db.Items.Remove(item);
+        await db.SaveChangesAsync();
+        return Results.Ok(item); // מחזיר את האובייקט שנמחק
+    }
+
+    return Results.NotFound(); // מחזיר NotFound אם האובייקט לא נמצא
+});
+
+
+app.Run();
+
+
+
